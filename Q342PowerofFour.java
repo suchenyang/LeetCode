8eef4e06bbe0063f7b403a352b275b07f7737361
Chenyang Su <chenyangsu@gmail.com>
2016-08-10 21:56:09 -0400

    add another solution for Q342

--- Q342PowerofFour.java
+++ Q342PowerofFour.java
@@ -1,7 +1,9 @@
+import java.lang.Math;
+
 public class Q342PowerofFour {
 	public static void main (String[] args) {
-		Solution s = new Solution();
-		if (s.isPowerofFour(-4)) {
+		Solution2 s = new Solution2();
+		if (s.isPowerofFour(16)) {
 			System.out.println("True");
 		}else {
 			System.out.println("False");
@@ -9,7 +11,7 @@ public class Q342PowerofFour {
 	}
 }
 
-class Solution {
+class Solution1 {
 	public boolean isPowerofFour(int num) {
 	//divide num by 4 again and again, if finally it gets to 1, it is a power of 4. Otherwise it is not.	
 	
@@ -24,4 +26,21 @@ class Solution {
 		}
 		return true;
 	}
-}  
+}
+
+class Solution2 {
+	public boolean isPowerofFour(int num) {
+	// If num are not positive integer, return false. Otherwise, take logarithm (base 4) and then round to the closest integer. Finally take the power (base 4) to compare with the original num. If the same, return true, otherwise return false.
+
+		if (num <= 0) {return false;}
+		double doublePower = Math.log10(num)/Math.log10(4);
+		long longPower = Math.round(doublePower);
+		double temp = Math.pow(4, longPower);
+		long longTemp = Math.round(temp);
+		if (longTemp == num) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+} 
